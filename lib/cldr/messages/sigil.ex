8392a0b6a18b8683ec95658f4620733c2f3acaf8
Kip Cole <kipcole9@gmail.com>
2021-12-09 05:48:55 +1100

    Add sigil_M and handle interpolation in sigil_m

--- lib/cldr/messages/sigil.ex
+++ lib/cldr/messages/sigil.ex
@@ -1,12 +1,6 @@
 defmodule Cldr.Message.Sigil do
 	@moduledoc """
-	Implements `sigil_m` nacro to canonicalize an
-	ICU message.
-
-	"""
-
-	@doc """
-	Implements sigil `~m` to canonicalize an
+	Implements sigil `~m` and sigil `~M` to canonicalize an
 	ICU message.
 
 	ICU messages allow for whitespace to be used to
@@ -23,14 +17,35 @@ defmodule Cldr.Message.Sigil do
 	`mix gettext.extract` however this may not be the desired
 	behaviour either.
 
-	The sigil `~m` therefore introduces a way for the developer
+	The sigils `~m` and `~M` therefore introduces a way for the developer
 	to ensure the message is in a canonical format during
 	compilation and therefore both error check the message format
 	and ensure the message is in a canonical form irrespective
 	of developer formatting.
 
 	"""
-	defmacro sigil_m({:<<>>, _meta, [message]}, modifiers) when is_binary(message) do
+
+  @doc ~S"""
+	Handles the sigil `~M` for ICU message strings.
+
+	It returns a a canonically formatted string without
+	interpolations and without escape characters, except
+	for the escaping of the closing sigil character
+	itself.
+
+	## Examples
+
+	    iex> ~m(An ICU message)
+	    "An ICU message"
+
+	However, if you want to re-use the sigil character itself on
+	the string, you need to escape it:
+
+	    iex> ~M((\))
+	    "()"
+
+	"""
+	defmacro sigil_M({:<<>>, _meta, [message]}, modifiers) when is_binary(message) do
 		options = Cldr.Message.Sigil.options(modifiers)
 		canonical_message = Cldr.Message.canonical_message!(message, options)
 
@@ -39,11 +54,55 @@ defmodule Cldr.Message.Sigil do
 		end
 	end
 
+  @doc ~S"""
+	Handles the sigil `~m` for ICU message strings.
+
+	It returns a canonically formatted string as if it
+	was a double quoted string, unescaping characters
+	and replacing interpolations.
+
+	## Examples
+
+	    iex> ~m(An ICU message)
+	    "An ICU message"
+
+	    iex> ~m(An ICU messag#{:e})
+	    "An ICU message"
+
+	"""
+	defmacro sigil_m({:<<>>, _meta, [message]}, modifiers) when is_binary(message) do
+		options = Cldr.Message.Sigil.options(modifiers)
+		message = Macro.unescape_string(message)
+		canonical_message = Cldr.Message.canonical_message!(message, options)
+
+		quote do
+			unquote(canonical_message)
+		end
+	end
+
+  defmacro sigil_m({:<<>>, meta, pieces}, modifiers) do
+		options = Cldr.Message.Sigil.options(modifiers)
+    message = {:<<>>, meta, unescape_tokens(pieces)}
+
+ 	 	quote do
+			Cldr.Message.canonical_message!(unquote(message), unquote(options))
+		end
+	end
+
+	@doc false
 	def options([pretty]) when pretty in [?p, ?P] do
 		[pretty: true]
 	end
 
 	def options(_modifiers) do
 		[]
+	end
+
+	@doc false
+  defp unescape_tokens(tokens) do
+	  Enum.map(tokens, fn
+	    token when is_binary(token) -> Macro.unescape_string(token)
+			other -> other
+		end)
 	end
 end
--- test/sigil_test.exs
+++ test/sigil_test.exs
@@ -2,6 +2,15 @@ defmodule Cldr.Message.Sigil.Test do
 	use ExUnit.Case, async: true
 	import Cldr.Message.Sigil
 
+	doctest Cldr.Message.Sigil
+
+	test "sigil_M" do
+	  assert ~M"{0} {1, plural,
+      one {est {2, select, female {allée} other  {allé}}}
+      other {sont {2, select, female {allées} other {allés}}}
+    } à {3}" == "{0} {1, plural, one {est {2, select, female {allée} other {allé}}} other {sont {2, select, female {allées} other {allés}}}} à {3}"
+	end
+
 	test "sigil_m" do
 	  assert ~m"{0} {1, plural,
       one {est {2, select, female {allée} other  {allé}}}
@@ -15,5 +24,13 @@ defmodule Cldr.Message.Sigil.Test do
       other {sont {2, select, female {allées} other {allés}}}
     } à {3}"p ==
 		"{0} {1, plural,\n  one {est \n    {2, select, \n      female {allée}\n      other {allé}}}\n  other {sont \n    {2, select, \n      female {allées}\n      other {allés}}}} à {3}"
+	end
+
+	test "sigil_m with interpolation" do
+		var = "{0}"
+	  assert ~m"#{var} {1, plural,
+      one {est {2, select, female {allée} other  {allé}}}
+      other {sont {2, select, female {allées} other {allés}}}
+    } à {3}" == "{0} {1, plural, one {est {2, select, female {allée} other {allé}}} other {sont {2, select, female {allées} other {allés}}}} à {3}"
 	end
 end
