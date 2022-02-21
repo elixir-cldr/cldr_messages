eecf8a735b8f03c85e1ee9d00a8ae0d799671a50
Kip Cole <kipcole9@gmail.com>
2022-02-22 02:14:44 +0800

    Fix compatibility with ex_cldr version 2.26

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Cldr.Messages.MixProject do
   use Mix.Project
 
-  @version "0.13.2"
+  @version "0.14.0"
 
   def project do
     [
@@ -44,23 +44,13 @@ defmodule Cldr.Messages.MixProject do
 
   defp deps do
     [
-      # {:nimble_parsec, "~> 0.5 or ~> 1.0"},
-      {:nimble_parsec, path: "../nimble_parsec", override: true},
+      {:ex_cldr_numbers, "~> 2.25"},
+      {:ex_cldr_dates_times, "~> 2.11", optional: true},
+      {:ex_money, "~> 5.9", optional: true},
+      {:ex_cldr_units, "~> 3.12", optional: true},
+      {:ex_cldr_lists, "~> 2.10", optional: true},
 
-      # {:ex_cldr_numbers, "~> 2.23"},
-      {:ex_cldr_numbers, path: "../cldr_numbers", override: true},
-
-      {:ex_cldr_dates_times, path: "../cldr_dates_times"},
-      {:ex_cldr_units, path: "../cldr_units"},
-      {:ex_cldr_lists, path: "../cldr_lists"},
-      {:ex_cldr_currencies, path: "../cldr_currencies", override: true},
-      {:ex_money, path: "../money"},
-
-      # {:ex_cldr_dates_times, "~> 2.10", optional: true},
-      # {:ex_money, "~> 5.7", optional: true},
-      # {:ex_cldr_units, "~> 3.8", optional: true},
-      # {:ex_cldr_lists, "~> 2.9", optional: true},
-
+      {:nimble_parsec, "~> 0.5 or ~> 1.0"},
       {:jason, "~> 1.1"},
       {:dialyxir, "~> 1.0", optional: true, only: [:dev], runtime: false},
       {:ex_doc, "~> 0.20", optional: true, runtime: false},
